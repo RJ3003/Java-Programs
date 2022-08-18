71ad5ca8e59ec6b58e4552a2f45e59bdce1a1d9e
Raj Jagtap <97020206+RJ3003@users.noreply.github.com>
2022-08-18 20:01:15 +0530

    Update table.java

--- table.java
+++ table.java
@@ -1,13 +1,16 @@
 package com.company;
+import java.util.Scanner;
 
 public class table {
     public static void main(String[] args) {
-        int n = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
         int product = 1;
 
         for (int i = 1; i<=10; i++){
             product = n * i;
-            System.out.println(n + "x" + i + "=" + product);
+            System.out.println(n + " x " + i + " = " + product);
         }
     }
 }
