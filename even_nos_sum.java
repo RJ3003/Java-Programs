c20a45a5de252b538464fe7696a8c97a81b64022
Raj Jagtap <97020206+RJ3003@users.noreply.github.com>
2022-08-23 23:06:39 +0530

    Add files via upload

---
+++ even_nos_sum.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class even_nos_sum {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=2;i<=20;i=i+2){
+            sum=sum+i;
+        }
+        System.out.println(sum );
+    }
+    }
+
