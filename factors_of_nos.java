90781e9d4ce95030510a21b1f96e15f662a3d6c6
Raj Jagtap <97020206+RJ3003@users.noreply.github.com>
2022-08-24 22:53:33 +0530

    Add files via upload

---
+++ factors_of_nos.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.*;
+
+public class factors_of_nos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i= 1; i<=n; i++){
+            if (n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
